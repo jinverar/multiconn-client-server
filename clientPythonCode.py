be1bc5cbda47ac6e318296e0f0ec7939b237b732
jinverar <jinverar@gmail.com>
2019-07-24 13:41:21 -0400

    Update clientPythonCode.py

--- clientPythonCode.py
+++ clientPythonCode.py
@@ -17,6 +17,10 @@ import socket
 #Finally, it provides some additional type-related utility classes and functions that are not fundamental enough to be builtins.
 import types
 
+
+#class selectors.DefaultSelector, The default selector class, using the most efficient implementation available on the current platform. 
+##This should be the default choice for most users.
+#put the DefaultSelector into a variable called "sel"
 sel = selectors.DefaultSelector()
 
 #below is my commands function. The user will enter commands and receive responses back. 
@@ -34,9 +38,25 @@ def commands():
         ui.console_textEdit.setText("[**] below are the main window help commands ")
         ui.console_textEdit.append("[+] Command options: ")
         ui.console_textEdit.append("[+] connect ====== > connect to the experimantal server")
+        ui.console_textEdit.append("[+] check ====== > check for connected clients")
+        ui.console_textEdit.append("[+] close ====== > disconnect from the server")
 
     if 'send' in command:
         messages = [b'Message 1 from client.', b'Message 2 from client.']
+
+    if 'close' in command:
+        sel.close()
+        ui.console_textEdit.append(" [**] closing connection to %s" % sel.register)
+
+    if 'check' in command:
+        #--------------------------------------------------------------------------------------------------
+        #clients = {'sip.voidptr object at 0x00000122031B2AD0' : 'PyQt5.QtNetwork.QTcpSocket object at 0x00000122031C53A8', 'sip.voidptr object at 0x00000122031B2B48': 'PyQt5.QtNetwork.QTcpSocket object at 0x00000122033D44C8'}
+        #-----------------------------------------------------------------------------------------------------
+        ui.console_textEdit.append(" [**] the selector objects are %s" % sel)
+        ui.console_textEdit.append(" [**] the keys are %s" % sel.get_key)
+        ui.console_textEdit.append(" [**] the map of clients are %s" % sel.get_map)
+        ui.console_textEdit.append(" [**] the select values are %s" % sel.select)
+        #ui.console_textEdit.append(" [**] client has connected and the socket objects are %s" % ui.conn)
 
 
 
@@ -70,6 +90,7 @@ def start_connections(address, port, num_conns):
         #increase the connid by 1 each time
         connid = i + 1
         print('starting connection', connid, 'to', server_addr)
+        ui.console_textEdit.append(" [**] connected")
         #create a socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         #set the socket to non blocking
@@ -94,10 +115,17 @@ def start_connections(address, port, num_conns):
         #those are directly added to the underlying namespace.
 
         # put type simple namespace into data and check the connid, if the connid is true then send the message if receive total is 0 check the list.
+        #data might be a named selector variable, Optional opaque data associated to this file object: for example, this could be used to store a per-client session ID.
         data = types.SimpleNamespace(connid=connid,msg_total=sum(len(m) for m in messages),recv_total=0,messages=list(messages),outb=b'')
+
+        #call the sel variable and register the selector with the sock.
+        #Events that must be waited for on this file object.
+        #data must equal data
         sel.register(sock, events, data=data)
 
 def service_connection(key, mask):
+    #File object registered.
+    #key is the SelectorKey instance corresponding to a ready file object. events is a bitmask of events ready on this file object.
     sock = key.fileobj
     data = key.data
     if mask & selectors.EVENT_READ:
